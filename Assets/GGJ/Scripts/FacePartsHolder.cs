0212aecb67f8c02b144983d45738ffff369107c2
sashimi-max <sashimi.shimi.sashimi@gmail.com>
2024-01-27 00:58:17 +0900

    add: FaceParts関連試作

---
+++ Assets/GGJ/Scripts/FacePartsHolder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(RectTransform))]
+public class FacePartsHolder : MonoBehaviour
+{
+    Keyboard current;
+    RectTransform rectTransform;
+    Sequence sequence;
+    float width;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = Keyboard.current;
+        var parentRectTransform = transform.parent.GetComponent<RectTransform>();
+        width = parentRectTransform.sizeDelta.x;
+        rectTransform = GetComponent<RectTransform>();
+        DoYoYo();
+    }
+
+    public void DoYoYo()
+    {
+        sequence = DOTween.Sequence();
+
+        sequence
+            .Append(rectTransform.DOAnchorPos(new Vector3(width, 0, 0), 1f))
+            .Append(rectTransform.DOAnchorPos(new Vector3(0, 0, 0), 1f))
+            .SetLoops(-1)
+            .Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current[Key.W].wasPressedThisFrame)
+        {
+            sequence.Kill();
+        }
+    }
+}
---
+++ Assets/GGJ/Scripts/FacePartsMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FacePartsMover : MonoBehaviour
+{
+    Keyboard current;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = Keyboard.current;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current[Key.W].wasReleasedThisFrame)
+        {
+            rb.AddRelativeForce(Vector2.up * 4.0f, ForceMode2D.Impulse);
+        }
+    }
+}
