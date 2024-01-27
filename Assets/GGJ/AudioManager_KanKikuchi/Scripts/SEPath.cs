a3ffdd16e01b629dc6b5cee84a1fd4886fc5ec43
sashimi-max <sashimi.shimi.sashimi@gmail.com>
2024-01-28 00:29:46 +0900

    add: se取り込み

--- Assets/GGJ/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ Assets/GGJ/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,9 +5,15 @@
 /// </summary>
 public static class SEPath{
 
-	public const string JINGLE10 = "SE/Jingle/jingle10";
-	public const string SYSTEM20 = "SE/system20";
-	public const string SYSTEM23 = "SE/system23";
+	public const string JINGLE10         = "SE/Jingle/jingle10";
+	public const string SE_FACE_RELEASE1 = "SE/ゲーム/SE_FaceRelease1";
+	public const string SE_FACE_RELEASE2 = "SE/ゲーム/SE_FaceRelease2";
+	public const string SE_FACE_SELECT1  = "SE/ゲーム/SE_FaceSelect1";
+	public const string SE_FACE_SELECT2  = "SE/ゲーム/SE_FaceSelect2";
+	public const string SE_FACE_SELECT3  = "SE/ゲーム/SE_FaceSelect3";
+	public const string SE_GAUGE         = "SE/ゲーム/SE_Gauge";
+	public const string SYSTEM20         = "SE/system20";
+	public const string SYSTEM23         = "SE/system23";
 
 }
 
