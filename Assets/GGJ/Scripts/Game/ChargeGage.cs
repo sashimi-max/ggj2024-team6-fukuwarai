992a70d8da2e9001814f982bcc0e684cabb197ad
sashimi-max <sashimi.shimi.sashimi@gmail.com>
2024-01-27 17:50:33 +0900

    add: chargeGageを追従するように

--- Assets/GGJ/Scripts/Game/ChargeGage.cs
+++ Assets/GGJ/Scripts/Game/ChargeGage.cs
@@ -6,28 +6,37 @@ using UnityEngine.UI;
 
 public class ChargeGage : MonoBehaviour
 {
+    [SerializeField] private AnimationCurve widthAnimationCurve;
+    [SerializeField] private AnimationCurve heightAnimationCurve;
+
     private PlayerInputManager playerInputManager;
     PlayerCharge playerCharge;
-    Slider slider;
+
+    private RectTransform view;
+
     // Start is called before the first frame update
     void Start()
     {
         playerCharge = GetComponentInParent<PlayerCharge>();
-        slider = GetComponent<Slider>();
-        slider.enabled = false;
+        view = GetComponentInChildren<Image>().gameObject.GetComponent<RectTransform>();
+        view.gameObject.SetActive(false);
         playerInputManager = GetComponentInParent<PlayerInputManager>();
         playerInputManager.OnPressedFireButton
-            .Subscribe(_ => slider.enabled = true)
+            .Subscribe(_ => view.gameObject.SetActive(true))
             .AddTo(this);
 
         playerInputManager.OnCanceledFireButton
-            .Subscribe(_ => slider.enabled = false)
+            .Subscribe(_ => view.gameObject.SetActive(false))
             .AddTo(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        slider.value = playerCharge.normalizedChargedTime;
+        if (!view.gameObject.activeSelf) return;
+        var width = widthAnimationCurve.Evaluate(playerCharge.normalizedChargedTime);
+        var height = heightAnimationCurve.Evaluate(playerCharge.normalizedChargedTime);
+        view.sizeDelta = new Vector2(width, height);
+        //v.value = playerCharge.normalizedChargedTime;
     }
 }
