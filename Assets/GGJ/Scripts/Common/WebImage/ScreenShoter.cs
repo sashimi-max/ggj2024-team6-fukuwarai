9cbc7a0d28693b83fb7370c7ea6c91e8ef5194fc
sashimi-max <sashimi.shimi.sashimi@gmail.com>
2024-02-03 21:38:00 +0900

    add: 最終版

--- Assets/GGJ/Scripts/Common/WebImage/ScreenShoter.cs
+++ Assets/GGJ/Scripts/Common/WebImage/ScreenShoter.cs
@@ -13,8 +13,6 @@ namespace GGJ.Common.WebImage
             // 描画が完了してからでないとエラーが出る
             await UniTask.WaitForEndOfFrame(this);
 
-            var w = rect.sizeDelta.x;
-            var h = rect.sizeDelta.y;
             var screenWidth = Screen.width;
             var screenHeight = Screen.height;
 #if UNITY_EDITOR
@@ -22,11 +20,15 @@ namespace GGJ.Common.WebImage
             screenWidth = int.Parse(res[0]);
             screenHeight = int.Parse(res[1]);
 #endif
-            var x = (int)(screenWidth / 2 - rect.sizeDelta.x / 2);
-            var y = (int)(screenHeight / 2 - rect.sizeDelta.y / 2);
+            var corners = new Vector3[4];
+            rect.GetWorldCorners(corners);
+            corners[0] = RectTransformUtility.WorldToScreenPoint(Camera.main, corners[0]);
+            var _s = screenWidth / 1920.0f; // 横長に合わせる
+            var w = rect.sizeDelta.x * _s;
+            var h = rect.sizeDelta.y * _s;
 
             var texture = new Texture2D((int)w, (int)h);
-            texture.ReadPixels(new Rect(x, y, w, h), 0, 0);
+            texture.ReadPixels(new Rect(corners[0].x, corners[0].y, w, h), 0, 0);
             texture.Apply();
             return texture;
         }
